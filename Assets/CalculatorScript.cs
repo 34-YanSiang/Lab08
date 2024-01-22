010b6184c59cab8bf78de920985a74b05d320a0b
34-YanSiang <155930750+34-YanSiang@users.noreply.github.com>
2024-01-22 12:57:34 +0800

    Updated Unity Project
    Unity Project is now a Currency Converter

---
+++ Assets/CalculatorScript.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalculatorScript : MonoBehaviour
+{
+    public GameObject USD;
+    public GameObject JPY;
+    public InputField txtAmount;
+    public InputField Convertedvalue;
+    private float SGDAmount;
+    private float value;
+    public void conversion()
+    {
+        try
+        {
+            SGDAmount = float.Parse(txtAmount.text);
+            if (USD.GetComponent<Toggle>().isOn == true)
+            {
+                if (JPY.GetComponent<Toggle>().isOn == true)
+                Convertedvalue.text = "Select Currency";
+                else
+                {
+                    value = SGDAmount * 0.74f;
+                    if (value >= 10000000)
+                    {
+                        value = value / 1000000;
+                        Convertedvalue.text = value.ToString() + "mil USD";
+                    }
+                    else
+                        Convertedvalue.text = value.ToString() + " USD";
+                }
+            }
+            else if (JPY.GetComponent<Toggle>().isOn == true)
+            {
+                value = SGDAmount * 110.5f;
+                if (value >= 10000000)
+                {
+                    value = value / 1000000;
+                    Convertedvalue.text = value.ToString() + "mil JPY";
+                }
+                else
+                    Convertedvalue.text = value.ToString() + " JPY";
+            }
+            else
+                Convertedvalue.text = "Select Currency";
+        }   
+        catch(FormatException)
+        {
+            Convertedvalue.text = "Invalid amount";
+        }
+    }
+    public void clear()
+    {
+        txtAmount.text = "";
+        USD.GetComponent<Toggle>().isOn = false;
+        JPY.GetComponent<Toggle>().isOn = false;
+        Convertedvalue.text = "";
+    }
+}
